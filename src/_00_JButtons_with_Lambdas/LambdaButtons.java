1ddb221c44a691b1190ab03e35defa8e9ece465e
opie10 <augie@keatinge.com>
2023-12-03 12:59:26 -0800

    ok
    I think I get lambdas, but theyre still kinda confusing. moving onto streams which are worse. Lambda buttons fun.

--- src/_00_JButtons_with_Lambdas/LambdaButtons.java
+++ src/_00_JButtons_with_Lambdas/LambdaButtons.java
@@ -1,11 +1,15 @@
 package _00_JButtons_with_Lambdas;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
-public class LambdaButtons {
+public class LambdaButtons implements ActionListener {
 	private JFrame window = new JFrame();
 	private JButton addNumbers = new JButton("ADD 2 Numbers");
 	private JButton randNumber = new JButton("RANDOM NUMBER");
@@ -20,7 +24,31 @@ public class LambdaButtons {
 		
 		//1. Call the addActionListener methods for each button. Use lambdas
 		//   to define to functionality of the buttons.
-		
+		addNumbers.addActionListener((s)->{
+			int num1 =Integer.parseInt(JOptionPane.showInputDialog("Input First Number:")) ;
+			int num2 =Integer.parseInt(JOptionPane.showInputDialog("Input Second Number:")) ;
+			JOptionPane.showMessageDialog(null, "The Result: "+(num1+num2));
+		});
+		randNumber.addActionListener((s)->{
+			Random rand = new Random();
+			int r = rand.nextInt(100000);
+			JOptionPane.showMessageDialog(null, "Here's your random number: "+r);
+		});
+		tellAJoke.addActionListener((s)->{
+			String joke ="You!(Joke)";
+			Random rand = new Random();
+			int r = rand.nextInt(4);
+			if (r==3) {
+				joke = "Where do polar bears keep their money? \nAnswer: In a snowbank!";
+			}
+			if (r==1) {
+				joke = "What would bears be without bees?\nAnswer: Ears!";
+			}
+			if (r==2) {
+				joke = "Why did the bicycle fall over? \nAnswer: Beacuse it was two tired!"	;	
+			}
+			JOptionPane.showMessageDialog(null, "Heres a joke:\n"+joke);
+		});
 		window.setVisible(true);
 		window.pack();
 	}
@@ -28,6 +56,15 @@ public class LambdaButtons {
 	public static void main(String[] args) {
 		new LambdaButtons();
 	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
 	
 	
 }
--- src/_01_Lambda_Methods/LambdaMethods.java
+++ src/_01_Lambda_Methods/LambdaMethods.java
@@ -13,13 +13,79 @@ public class LambdaMethods {
 		}, "repeat");
 		
 		//2. Call the printCustonMessage method using a lambda so that the String prints backwards.
-		
+		printCustomMessage((s)->{
+			String t="";
+			for(int i = s.length()-1; i >= 0; i--) {
+				t+=s.charAt(i);
+				
+			}
+			System.out.println(t);
+			
+		}, "te");
 		//3. Call the printCustonMessage method using a lambda so that the String prints with a mix between upper an lower case characters.
-		
+		printCustomMessage((s)->{
+			String t="";
+			for(int i = 0; i <s.length(); i++) {
+				if (i%2==0) {
+					Character up = s.charAt(i);
+					String uper =up.toString().toUpperCase();
+					
+					t= t+uper;
+				}
+				else {
+
+					t+=s.charAt(i);
+				
+			}
+			
+			
+		}System.out.println(t);
+			}, "testing");
 		//4. Call the printCustonMessage method using a lambda so that the String prints with a period in between each character.
-		
+		printCustomMessage((s)->{
+			String t="";
+			for(int i = 0; i <s.length(); i++) {
+				t+=s.charAt(i)+".";
+			
+			
+		}System.out.println(t);
+			}, "testing");
 		//5. Call the printCustonMessage method using a lambda so that the String prints without any vowels.
-	
+		printCustomMessage((s)->{
+			
+			String t="";
+			for(int i = 0; i <s.length(); i++) {
+				Boolean vow = false;
+				 switch (s.charAt(i)) {
+			        case 'a':  
+			        	 vow=true;
+			        case 'e':
+			        	 vow=true;
+			        case 'i':
+			        	 vow=true;
+			        case 'o':
+			        	 vow=true;
+			        case 'u':
+			        	 vow=true;
+			        case 'A':
+			        	 vow=true;
+			        case 'E': 
+			        	 vow=true;
+			        case 'I': 
+			        	 vow=true;
+			        case 'O':  
+			        	 vow=true;
+			        case 'U':
+			            vow=true;
+			            
+			        
+			    }
+				
+				 t= (vow)?(t):(t+=s.charAt(i));
+
+			
+		}System.out.println(t);
+			}, "testing");
 	}
 	
 	public static void printCustomMessage(SpecialPrinter sp, String value) {
